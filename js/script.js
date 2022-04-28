08f64ecb85b4d90a122eea2f13364eb24be9fcd6
ll560 <llavia6@gmail.com>
2022-04-28 18:00:41 -0400

    fixed on and off toogle

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
 function myFunction(){
     let x = document.getElementById("light-on");
     if (x.style.display === "block") {
-        x.style.display = "inline";
-        //x.setAttribute("src", "/images/off-light-bulb-png.png");
+        x.style.display = "inline-block";
+        x.setAttribute("src", "/images/off-light-bulb-png.png");
         
       } else {
-        x.style.display !== "block";
+        if(x.style.display = "inline-block"){
+        x.style.display = "block"
         document.createElement("newImg");
         x.setAttribute("src", "/images/on-light-bulb-png-transparent.png");
-        
+        }
       }
 }
+
+// function myFunction(){
+//     let x = document.getElementById("light-on");
+//     if (x.style.display === "block") {
+//         x.style.display = "inline";
+//         //x.setAttribute("src", "/images/off-light-bulb-png.png");
+        
+//       } else {
+//         if(x.style.display = "block"){
+//         //x.style.display === "block"
+//         document.createElement("newImg");
+//         x.setAttribute("src", "/images/on-light-bulb-png-transparent.png");
+//         }
+//       }
+// }
